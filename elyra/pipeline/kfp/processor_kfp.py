a739eb7d205de669c92b61f94323165f47fcd411
rkpattnaik780 <rkpattnaik780@gmail.com>
2024-02-27 19:25:05 +0530

    current state

--- elyra/pipeline/kfp/processor_kfp.py
+++ elyra/pipeline/kfp/processor_kfp.py
@@ -42,9 +42,14 @@ from jinja2 import PackageLoader
 from kfp import Client as ArgoClient
 from kfp import compiler as kfp_argo_compiler
 from kfp import components as components
+from kfp.dsl import kfp_config
+from kfp.kubernetes import common
+from kfp.kubernetes import kubernetes_executor_config_pb2 as pb
 from kubernetes import client as k8s_client
 from traitlets import default
 from traitlets import Unicode
+
+from google.protobuf import json_format
 
 RUN_ID_PLACEHOLDER = "random-placeholder"
 
@@ -108,162 +113,6 @@ CRIO_VOL_DEF_MEDIUM = ""
 CRIO_VOL_MOUNT_PATH = "/opt/app-root/src"
 CRIO_VOL_WORKDIR_PATH = f"{CRIO_VOL_MOUNT_PATH}/jupyter-work-dir"
 CRIO_VOL_PYTHON_PATH = f"{CRIO_VOL_WORKDIR_PATH}/python3"
-
-class PipelineConf():
-    """PipelineConf contains pipeline level settings."""
-
-    def __init__(self):
-        self.image_pull_secrets = []
-        self.timeout = 0
-        self.ttl_seconds_after_finished = -1
-        self._pod_disruption_budget_min_available = None
-        self.op_transformers = []
-        self.default_pod_node_selector = {}
-        self.image_pull_policy = None
-        self.parallelism = None
-        self._data_passing_method = None
-        self.dns_config = None
-
-    def set_image_pull_secrets(self, image_pull_secrets):
-        """Configures the pipeline level imagepullsecret.
-
-        Args:
-          image_pull_secrets: a list of Kubernetes V1LocalObjectReference For
-            detailed description, check Kubernetes V1LocalObjectReference definition
-            https://github.com/kubernetes-client/python/blob/master/kubernetes/docs/V1LocalObjectReference.md
-        """
-        self.image_pull_secrets = image_pull_secrets
-        return self
-
-    def set_timeout(self, seconds: int):
-        """Configures the pipeline level timeout.
-
-        Args:
-          seconds: number of seconds for timeout
-        """
-        self.timeout = seconds
-        return self
-
-    def set_parallelism(self, max_num_pods: int):
-        """Configures the max number of total parallel pods that can execute at
-        the same time in a workflow.
-
-        Args:
-          max_num_pods: max number of total parallel pods.
-        """
-        if max_num_pods < 1:
-            raise ValueError(
-                'Pipeline max_num_pods set to < 1, allowed values are > 0')
-
-        self.parallelism = max_num_pods
-        return self
-
-    def set_ttl_seconds_after_finished(self, seconds: int):
-        """Configures the ttl after the pipeline has finished.
-
-        Args:
-          seconds: number of seconds for the workflow to be garbage collected after
-            it is finished.
-        """
-        self.ttl_seconds_after_finished = seconds
-        return self
-
-    def set_pod_disruption_budget(self, min_available: Union[int, str]):
-        """PodDisruptionBudget holds the number of concurrent disruptions that
-        you allow for pipeline Pods.
-
-        Args:
-          min_available (Union[int, str]):  An eviction is allowed if at least
-            "minAvailable" pods selected by "selector" will still be available after
-            the eviction, i.e. even in the absence of the evicted pod.  So for
-            example you can prevent all voluntary evictions by specifying "100%".
-            "minAvailable" can be either an absolute number or a percentage.
-        """
-        self._pod_disruption_budget_min_available = min_available
-        return self
-
-    def set_default_pod_node_selector(self, label_name: str, value: str):
-        """Add a constraint for nodeSelector for a pipeline.
-
-        Each constraint is a key-value pair label.
-
-        For the container to be eligible to run on a node, the node must have each
-        of the constraints appeared as labels.
-
-        Args:
-          label_name: The name of the constraint label.
-          value: The value of the constraint label.
-        """
-        self.default_pod_node_selector[label_name] = value
-        return self
-
-    def set_image_pull_policy(self, policy: str):
-        """Configures the default image pull policy.
-
-        Args:
-          policy: the pull policy, has to be one of: Always, Never, IfNotPresent.
-            For more info:
-            https://github.com/kubernetes-client/python/blob/10a7f95435c0b94a6d949ba98375f8cc85a70e5a/kubernetes/docs/V1Container.md
-        """
-        self.image_pull_policy = policy
-        return self
-
-    def add_op_transformer(self, transformer):
-        """Configures the op_transformers which will be applied to all ops in
-        the pipeline. The ops can be ResourceOp, VolumeOp, or ContainerOp.
-
-        Args:
-          transformer: A function that takes a kfp Op as input and returns a kfp Op
-        """
-        self.op_transformers.append(transformer)
-
-    def set_dns_config(self, dns_config: V1PodDNSConfig):
-        """Set the dnsConfig to be given to each pod.
-
-        Args:
-          dns_config: Kubernetes V1PodDNSConfig For detailed description, check
-            Kubernetes V1PodDNSConfig definition
-            https://github.com/kubernetes-client/python/blob/master/kubernetes/docs/V1PodDNSConfig.md
-
-        Example:
-          ::
-
-            import kfp
-            from kubernetes.client.models import V1PodDNSConfig, V1PodDNSConfigOption
-            pipeline_conf = kfp.dsl.PipelineConf()
-            pipeline_conf.set_dns_config(dns_config=V1PodDNSConfig(
-                nameservers=["1.2.3.4"],
-                options=[V1PodDNSConfigOption(name="ndots", value="2")],
-            ))
-        """
-        self.dns_config = dns_config
-
-    @property
-    def data_passing_method(self):
-        return self._data_passing_method
-
-    @data_passing_method.setter
-    def data_passing_method(self, value):
-        """Sets the object representing the method used for intermediate data
-        passing.
-
-        Example:
-          ::
-
-            from kfp.dsl import PipelineConf, data_passing_methods
-            from kubernetes.client.models import V1Volume, V1PersistentVolumeClaimVolumeSource
-            pipeline_conf = PipelineConf()
-            pipeline_conf.data_passing_method =
-            data_passing_methods.KubernetesVolume(
-                volume=V1Volume(
-                    name='data',
-                    persistent_volume_claim=V1PersistentVolumeClaimVolumeSource('data-volume'),
-                ),
-                path_prefix='artifact_data/',
-            )
-        """
-        self._data_passing_method = value
-
 class KfpPipelineProcessor(RuntimePipelineProcessor):
     _type = RuntimeProcessorType.KUBEFLOW_PIPELINES
     _name = "kfp"
@@ -410,7 +259,7 @@ class KfpPipelineProcessor(RuntimePipelineProcessor):
 
         with tempfile.TemporaryDirectory() as temp_dir:
             self.log.debug(f"Created temporary directory at: {temp_dir}")
-            pipeline_path = os.path.join(temp_dir, f"{pipeline_name}.tar.gz")
+            pipeline_path = os.path.join(temp_dir, f"{pipeline_name}.yaml")
 
             #############
             # Get Pipeline ID
@@ -486,7 +335,7 @@ class KfpPipelineProcessor(RuntimePipelineProcessor):
                     )
 
                     # extract the ID of the pipeline we created
-                    pipeline_id = kfp_pipeline.id
+                    pipeline_id = kfp_pipeline.pipeline_id
 
                     # the initial "pipeline version" has the same id as the pipeline itself
                     version_id = pipeline_id
@@ -501,7 +350,7 @@ class KfpPipelineProcessor(RuntimePipelineProcessor):
                     )
 
                     # extract the id of the "pipeline version" that was created
-                    version_id = kfp_pipeline.id
+                    version_id = kfp_pipeline.pipeline_id
 
             except Exception as ex:
                 # a common cause of these errors is forgetting to include `/pipeline` or including it with an 's'
@@ -551,7 +400,7 @@ class KfpPipelineProcessor(RuntimePipelineProcessor):
 
                 # create pipeline run (or specified pipeline version)
                 run = client.run_pipeline(
-                    experiment_id=experiment.id, job_name=job_name, pipeline_id=pipeline_id, version_id=version_id
+                    experiment_id=experiment.experiment_id, job_name=job_name, pipeline_id=pipeline_id, version_id=version_id
                 )
 
             except Exception as ex:
@@ -766,7 +615,7 @@ class KfpPipelineProcessor(RuntimePipelineProcessor):
         return pipeline_dsl
 
     def _compile_pipeline_dsl(
-        self, dsl: str, workflow_engine: WorkflowEngineType, output_file: str, pipeline_conf: PipelineConf
+        self, dsl: str, workflow_engine: WorkflowEngineType, output_file: str, pipeline_conf: kfp_config.KFPConfig
     ) -> None:
         """
         Compile Python DSL using the compiler for the specified workflow_engine.
@@ -801,7 +650,7 @@ class KfpPipelineProcessor(RuntimePipelineProcessor):
                 # in the generated Python DSL "generated_pipeline"
                 pipeline_function = getattr(mod, "generated_pipeline")
                 # compile the DSL
-                kfp_argo_compiler.Compiler().compile(pipeline_function, output_file, pipeline_conf=pipeline_conf)
+                kfp_argo_compiler.Compiler().compile(pipeline_function, output_file)
             except Exception as ex:
                 raise RuntimeError(
                     f"Failed to compile pipeline with workflow_engine '{workflow_engine.value}' to '{output_file}'"
@@ -1147,7 +996,7 @@ class KfpPipelineProcessor(RuntimePipelineProcessor):
         self.log_pipeline_info(pipeline_name, "Pipeline processed", duration=(time.time() - t0_all))
         return workflow_tasks
 
-    def _generate_pipeline_conf(self, pipeline: Pipeline) -> PipelineConf:
+    def _generate_pipeline_conf(self, pipeline: Pipeline) -> kfp_config.KFPConfig:
         """
         Returns a KFP pipeline configuration for this pipeline, which can be empty.
 
@@ -1156,7 +1005,7 @@ class KfpPipelineProcessor(RuntimePipelineProcessor):
         """
 
         self.log.debug("Generating pipeline configuration ...")
-        pipeline_conf = PipelineConf()
+        pipeline_conf = kfp_config.KFPConfig()
 
         #
         # Gather input for container image pull secrets in support of private container image registries
@@ -1182,11 +1031,41 @@ class KfpPipelineProcessor(RuntimePipelineProcessor):
             for secret_name in list(set(container_image_pull_secret_names)):
                 container_image_pull_secrets.append(k8s_client.V1ObjectReference(name=secret_name))
             pipeline_conf.set_image_pull_secrets(container_image_pull_secrets)
+
+            
+
             self.log.debug(
                 f"Added {len(container_image_pull_secrets)}" " image pull secret(s) to the pipeline configuration."
             )
 
         return pipeline_conf
+
+    def set_image_pull_secrets(
+        task: kfp_config.KFPConfig,
+        secret_names: List[str],
+    ) -> kfp_config.KFPConfig:
+        """Set image pull secrets for a Kubernetes task.
+
+        Args:
+            task: Pipeline task.
+            secret_names: List of image pull secret names.
+
+        Returns:
+            Task object with updated image pull secret configuration.
+        """
+
+        msg = common.get_existing_kubernetes_config_as_message(task)
+
+        # Assuming secret_names is a list of strings
+        image_pull_secret = [
+            pb.ImagePullSecret(secret_name=secret_name) for secret_name in secret_names
+        ]
+
+        msg.image_pull_secret.extend(image_pull_secret)
+
+        task.platform_config['kubernetes'] = json_format.MessageToDict(msg)
+
+        return task
 
     def _compose_container_command_args(
         self,
